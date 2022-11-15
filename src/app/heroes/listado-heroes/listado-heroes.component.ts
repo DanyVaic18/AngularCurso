0c170cf4b9a41124ff36f6388d02af60d1810285
DanyVaic18 <loljose1090@gmail.com>
2022-11-15 18:21:18 -0500

    Modificando esticlo con NgStyle

--- src/app/heroes/listado-heroes/listado-heroes.component.ts
+++ src/app/heroes/listado-heroes/listado-heroes.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface EstiloBtnDIsabled {
+  color: string;
+  'background-color': string;
+}
 @Component({
   selector: 'app-listado-heroes',
   templateUrl: './listado-heroes.component.html',
   // styleUrls: ['./listado-heroes.component.css']
 })
-export class ListadoHeroesComponent{
-  verdadero: string|number = 10
-  heroes : string[] = [
+export class ListadoHeroesComponent {
+  verdadero: string | number = 10;
+  heroes: string[] = [
     'SPIDER-MAN',
     'IRON-MAN',
     'HULK',
     'THOR',
     'SUPER-MAN',
-    'MR-INCREIBLE'
-  ]
-  heroeBorrado:string = 'No se ah borrado ningun heroe'
-  heroeAgregado:string = ''
+    'MR-INCREIBLE',
+  ];
+  heroeBorrado: string = 'No se ah borrado ningun heroe';
+  heroeAgregado: string = '';
+  colorObj: EstiloBtnDIsabled = {
+    color: '#FFFFFF',
+    'background-color': '#000',
+  };
   listHeroes: string[] = [
     'SPIDER-MAN',
     'IRON-MAN',
@@ -29,26 +37,33 @@ export class ListadoHeroesComponent{
     'CAPITAN-AMERICA',
     'DOCTOR-STRANGE',
     'NICK-FIURE',
-  ]
-  btnAggDisabled: boolean = false
-  agregarHeroe (){
-    if(this.listHeroes.length === this.heroes.length){ //
-      this.btnAggDisabled = true
-    }else{
-      this.heroes.push(this.listHeroes[this.heroes.length])
-      this.heroeAgregado = this.heroes[this.heroes.length-1]
+  ];
+  btnAggDisabled: boolean = false;
+  agregarHeroe() {
+
+
+    if (this.listHeroes.length === this.heroes.length) {
+      this.btnAggDisabled = true;
+    } else {
+      this.heroes.push(this.listHeroes[this.heroes.length]);
+      this.heroeAgregado = this.heroes[this.heroes.length - 1];
+      this.listHeroes.length === this.heroes.length ?  this.colorObj['background-color'] = '#FF0101':
+      this.colorObj['background-color'] = '#000'
     }
+
     /*
      let numAletorio:number = Math.floor(Math.random()*listHeroes.length)
      if(!this.heroes.includes(listHeroes[numAletorio]))this.heroes.push( listHeroes[numAletorio])
     */
   }
-  borrarHeroe(){
+  borrarHeroe() {
     //let heroeBorrado = this.heroes.pop() // Almacena el herue eliminado del array
-    this.heroeBorrado = this.heroes.pop() || 'No se ah borrado ningun heroe'
-    if(this.listHeroes.length !== this.heroes.length)this.btnAggDisabled = false
+    this.heroeBorrado = this.heroes.pop() || 'No se ah borrado ningun heroe';
+    if (this.listHeroes.length !== this.heroes.length)
+      this.btnAggDisabled = false;
+      this.colorObj['background-color'] = '#000';
   }
-  vaciarListaHeroes (){
-    this.heroes = []
+  vaciarListaHeroes() {
+    this.heroes = [];
   }
 }
