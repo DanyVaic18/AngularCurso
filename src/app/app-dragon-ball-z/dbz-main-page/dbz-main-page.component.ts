ac80a8b63b413b653243f618d72b0c4ae501811b
Laura <lauradanielapg97@gmail.com>
2022-11-17 21:51:48 -0500

    Lista de Personajes de Dbz

--- src/app/app-dragon-ball-z/dbz-main-page/dbz-main-page.component.ts
+++ src/app/app-dragon-ball-z/dbz-main-page/dbz-main-page.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 
+import { Personaje } from '../interface/personaje';
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './dbz-main-page.component.html',
   styleUrls: ['./dbz-main-page.component.css'],
 })
 export class DbzMainPageComponent {
+
+  personajesDbz:Personaje[] = [
+    {
+      nombre:"Goku",
+      poder:200,
+    },
+    {
+      nombre:"Vegeta",
+      poder:180,
+    },
+    {
+      nombre:"Frezzer",
+      poder:210,
+    },
+  ]
+
+  caracteristicas: Personaje = {
+    nombre: '',
+    poder: '',
+  };
+
+  cambiarNombre(ev:any){
+    console.log(ev.target.value)
+  }
+
   agregar() {
-    console.log("vida loca")
+    const caracteristicasIniciales:Personaje = {
+      nombre: '',
+      poder: '',
+    }
+    if(this.caracteristicas.nombre.trim().length && this.caracteristicas.poder === null){return;}
+    this.personajesDbz.push(this.caracteristicas)
+    this.caracteristicas = caracteristicasIniciales
+
   }
 }
